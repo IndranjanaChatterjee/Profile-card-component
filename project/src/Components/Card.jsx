673c16a36e1c473f54e8943b6a56ab62632fa43a
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-01-17 21:05:48 +0530

    card

--- project/src/Components/Card.jsx
+++ project/src/Components/Card.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import up from "../assets/images/bg-pattern-card.svg";
+import victor from "../assets/images/image-victor.jpg";
 
 export default function Card() {
   return (
-    <div>
-      card
+    <div className="relative bg-green-300 w-[20rem] h-[30rem]">
+      <img src={up} alt="img" className="w-[20rem]" />
+      <img
+        src={victor}
+        alt=""
+        className="rounded-full border-[5px] border-solid border-[#ffffff] m-[auto] relative top-[-3rem]"
+      />
+      <span>
+        <p>Victor Crest</p>
+        <p> 26</p>
+      </span>
+      <span>London</span>
+      <hr />
+      <div className="follow">
+        <div className="details">
+          <h1>80K</h1>
+          <p>Followers</p>
+        </div>
+        <div className="details">
+          <h1>803K</h1>
+          <p>Likes</p>
+        </div>
+        <div className="details">
+          <h1>1.4K
+  </h1>
+          <p>Photos</p>
+        </div>
+        
+      </div>
     </div>
-  )
+  );
 }
