1206a13f6321517ee9dfa3cf6f139e121a8637c5
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-01-17 20:40:00 +0530

    background

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import Card from "./Components/Card";
+import bottom from "../src/assets/images/bg-pattern-bottom.svg";
+import top from "../src/assets/images/bg-pattern-top.svg";
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Card/>
+      <section className="min-h-[100vh] relative overflow-hidden flex justify-center items-center">
+        <img src={bottom} alt="img" className="absolute sm:bottom-[-33rem] sm:right-[-13rem] bottom-[-7rem] right-[-12rem]"/>
+        <img src={top} alt="img" className="absolute sm:top-[-33rem] sm:left-[-13rem] top-[-12rem] left-[-12rem]"/>
+        <Card />
+      </section>
     </>
   );
 }
