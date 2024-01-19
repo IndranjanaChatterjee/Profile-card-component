8900eae4180faad99f5d511388edacd720ed504c
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-01-19 19:59:10 +0530

    finish

--- project/src/Components/Card.jsx
+++ project/src/Components/Card.jsx
@@ -11,24 +11,24 @@ export default function Card() {
         alt=""
         className="rounded-full border-[5px] border-solid border-[#ffffff] mx-[auto] relative top-[-3rem] "
       />
-      <span className="flex flex-row justify-center items-center gap-[0.5rem]  relative bottom-[1rem]">
-        <p>Victor Crest</p>
-        <p> 26</p>
+      <span className="flex flex-row justify-center items-center gap-[0.5rem]  relative bottom-[1.8rem] ">
+        <p className="font-extrabold text-[#2d3248]">Victor Crest</p>
+        <p className="text-[#6a6f81] font-normal"> 26</p>
       </span>
-      <span className="flex flex-row justify-center items-center relative bottom-[0.5rem]">London</span>
+      <span className="flex flex-row justify-center items-center relative bottom-[1.4rem] text-[#6a6f81]">London</span>
       
-      <div className="follow flex flex-row justify-center items-center gap-[3rem]  p-[1.2rem] border-t-[0.1rem] border-t-solid border-t-[#000000]">
-        <div className="details flex flex-col justify-center items-center">
+      <div className="follow flex flex-row justify-center items-center gap-[3rem]  p-[1.2rem] border-t-[0.05rem] border-t-solid border-t-[#e9e9e9] ">
+        <div className="details flex flex-col justify-center items-center font-extrabold text-[#2d3248]">
           <h1>80K</h1>
-          <p>Followers</p>
+          <p className="text-[#6a6f81] font-normal text-[0.6rem] tracking-[0.15rem]">Followers</p>
         </div>
-        <div className="details flex flex-col justify-center items-center">
+        <div className="details flex flex-col justify-center items-center font-extrabold text-[#2d3248]">
           <h1>803K</h1>
-          <p>Likes</p>
+          <p className="text-[#6a6f81] font-normal text-[0.6rem] tracking-[0.15rem]">Likes</p>
         </div>
-        <div className="details flex flex-col justify-center items-center">
+        <div className="details flex flex-col justify-center items-center font-extrabold text-[#2d3248]">
           <h1>1.4K</h1>
-          <p>Photos</p>
+          <p className="text-[#6a6f81] font-normal text-[0.6rem] tracking-[0.15rem]">Photos</p>
         </div>
       </div>
     </div>
