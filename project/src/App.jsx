3f1f6162a9ea5e62fd8038f8aea3e4c10cfce82c
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-01-19 20:08:59 +0530

    changes

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      <section className="min-h-[100vh] relative overflow-hidden flex justify-center items-center">
+      <section className="min-h-[100vh] relative overflow-hidden flex flex-col justify-center items-center">
         <img src={bottom} alt="img" className="absolute sm:bottom-[-33rem] sm:right-[-13rem] bottom-[-7rem] right-[-12rem]"/>
         <img src={top} alt="img" className="absolute sm:top-[-33rem] sm:left-[-13rem] top-[-12rem] left-[-12rem]"/>
         <Card />
--- project/src/Components/Attribution.jsx
+++ project/src/Components/Attribution.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Attribution() {
   return (
-    <div className="attribution">
+    <div className="attribution m-[2rem] bg-[#000000] p-[0.5rem] rounded-[1rem]">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
     Coded by <a href="#">IndranjanaChatterjee</a>.
   </div>
