bdb560a9ad5fa683881194b53f2a3cba07117c25
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-01-19 20:02:32 +0530

    attribution

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Card from "./Components/Card";
 import bottom from "../src/assets/images/bg-pattern-bottom.svg";
 import top from "../src/assets/images/bg-pattern-top.svg";
+import Attribution from "./Components/Attribution";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <img src={bottom} alt="img" className="absolute sm:bottom-[-33rem] sm:right-[-13rem] bottom-[-7rem] right-[-12rem]"/>
         <img src={top} alt="img" className="absolute sm:top-[-33rem] sm:left-[-13rem] top-[-12rem] left-[-12rem]"/>
         <Card />
+        <Attribution/>
       </section>
     </>
   );
